57298a1bd5aaa14475d99358a55c00d61875ed64
Sunny9252 <0913ing@gmail.com>
2020-04-24 14:45:09 +0900

    Hello Git

---
+++ JavaScript/testjs.js
@@ -0,0 +1,19 @@
+// document.write("Hello 🎈");
+
+// function printDate() {
+
+//     document.getElementById("date").innerHTML = Date();
+
+// }
+
+// Creating variables
+var name = "Giant Peng";
+var age = 10;
+var icon = "🐱‍👤";
+
+document.write(name);
+document.write("<br>");
+document.write(age);
+document.write("<br>");
+document.write(icon);
+document.write("<br>");
